1eee3d09c13d93ac1af50d90c60bdbb9f34226b3
M.hammad abbasi <161203782+hammad102030@users.noreply.github.com>
2024-08-31 17:17:20 +0500

    re-change

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
-import Style from "../style/style";
+import { FC } from "react";
 
-function Header() {
+const Header: FC = () => {
   return (
     <header className="bg-black text-white py-2">
       <div className="flex items-center justify-between px-5 py-2 m-2 space-x-4 rounded-full font-bold">
-      
+        {/* Logo or Home Link */}
         <Link href="/" className="flex items-center space-x-4">
           <h1 className="text-2xl md:text-3xl font-bold italic hover:underline underline-offset-2">
             HA
           </h1>
         </Link>
 
+        {/* Navigation Links */}
         <nav className="space-x-4 hidden md:flex">
           <Link href="/blog" className="hover:underline underline-offset-2">
             Blog
@@ -23,14 +24,21 @@ function Header() {
             Contact
           </Link>
         </nav>
-       
-        
-        
+
+        {/* Mobile Menu Button (Optional) */}
+        <div className="flex md:hidden">
+          <button
+            className="p-2 text-text2"
+            aria-label="Open Menu"
+            
+          >
+            ☰
+          </button>
         </div>
-      
+        
+      </div>
     </header>
-    
-  )
-}
+  );
+};
 
 export default Header;
